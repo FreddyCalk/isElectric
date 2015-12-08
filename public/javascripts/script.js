f7fbaf7eceb2bcc5ffe584219d3052751645ca3a
Freddy Calk <freddycalk@gmail.com>
2015-12-08 18:00:30 -0500

    added the buttons
    the buttons either add the car to your list of dream cars, or it
    ignores and discards the photo… The cars still repeat themselves during
    the voting stage, but that is the next item to fix

---
+++ public/javascripts/script.js
@@ -0,0 +1,4 @@
+function nextCar(type){
+	photo.splice(rand,1);
+	rand = Math.floor(Math.random()*photo.length);
+}
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,24 @@ var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	MongoClient.connect('mongodb://localhost:27017/mongotest', function (error, db){
-		db.collection('teams').find().toArray(function (error, result){
-			console.log(result);
-		})
-	});
+	var currIP = req.ip;
+	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+		db.collection('users').find({ip:currIP}).toArray(function (error, userResult){
+			var photosVoted = [];
+			for(i=0;i<userResult.length;i++){
+				photosVoted.push(userResult[i].image.src);
+			}
+			db.collection('cars').find({photo: {$nin:photosVoted}}).toArray(function (error, result){
+				if(result.length == 0){
+					// redirect to the thanks page
+					// res.render('index',{photo: result})
+				}else{
+					var rand = Math.floor(Math.random()*result.length)
+					res.render('index',{ photo: result[rand] })
+				}
+
+			});
+		});
 	// Index page should load random picture/item
 	// 1. Get all pictures from the MongoDB
 	// 2. Get the current user from MongoDB via req.ip;
@@ -16,22 +29,42 @@ router.get('/', function(req, res, next) {
 	// 4. Load all of those photos into an array.
 	// 5. Choose a random image from the array, and set it to a var.
 	// 6. res.render() the index view and send it the photo.
-
-	var serverPhotos = [{name: "http://www.boston.com/cars/newsandreviews/overdrive/2010/04/27/Chad-Conway-Comuta-Car.jpg"},
-	{name: "https://i.ytimg.com/vi/dJfSS0ZXYdo/hqdefault.jpg"}];
-
-	res.render('index', { photo: serverPhotos });
+	
+	});
 });
 
-router.get('/standings', function (req, res, next){
+router.get('/favorites', function (req, res, next){
+	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+		db.collection('users').find({vote: 'favorites'}).toArray(function (error, result){
+			console.log(result)
+			res.render('favorites',{title: "Standings", photos : result})
+		})
+	})
 	// 1. get all the photos
 	// 2. sort them by most likes
 	// 3. res.render the standings view and pass it the sorted photo array.
-	res.render('index',{title: "Standings"})
 })
 
-router.post('*', function (req, res, next){
-	// this will run for all posted pages.
+router.post('/favorites', function (req, res, next){
+	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+		db.collection('users').insertOne({
+			ip: req.ip,
+			vote: 'favorites',
+			image: req.body
+		})
+	})
+	res.redirect('../');
+})
+
+router.post('/pass', function (req, res, next){
+	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+		db.collection('users').insertOne({
+			ip: req.ip,
+			vote: 'pass',
+			image: req.body
+		})
+	})
+	res.redirect('../');
 });
 
 module.exports = router;
