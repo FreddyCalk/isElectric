6424aa56e9d2bc6d1fd111bc5422979947638054
Freddy Calk <freddycalk@gmail.com>
2015-12-09 12:23:33 -0500

    added logic for standings
    adding car to dream cars up votes the car, and passing on it down votes
    the car. The standings page will display all of the cars in order

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
+var mongoURL = 'mongodb://localhost:27017/cardb';
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	var currIP = req.ip;
-	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+	MongoClient.connect(mongoURL, function (error, db){
 		db.collection('users').find({ip:currIP}).toArray(function (error, userResult){
 			var photosVoted = [];
 			for(i=0;i<userResult.length;i++){
@@ -33,7 +35,7 @@ router.get('/', function (req, res, next) {
 	});
 })
 router.get('/favorites', function (req, res, next){
-	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+	MongoClient.connect(mongoURL, function (error, db){
 		db.collection('users').find({vote: 'favorites'}).toArray(function (error, result){
 			res.render('favorites',{title: "Standings", photos : result})
 		})
@@ -43,14 +45,19 @@ router.get('/favorites', function (req, res, next){
 	// 3. res.render the standings view and pass it the sorted photo array.
 })
 router.get('/list', function (req, res, next){
-	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+	MongoClient.connect(mongoURL, function (error, db){
 		db.collection('cars').find().toArray(function (error, result){
+			
+			result.sort(function (p1, p2){
+				return (p2.totalVotes - p1.totalVotes);
+			})
+
 			res.render('list',{photos : result})
 		})
 	})
 })
 router.get('/losers', function (req, res, next){
-	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
+	MongoClient.connect(mongoURL, function (error, db){
 		db.collection('users').find({vote: 'pass'}).toArray(function (error, result){
 			res.render('losers',{photos : result});
 		})
@@ -60,28 +67,61 @@ router.get('/losers', function (req, res, next){
 router.get('/add', function (req, res, next){
 	res.render('add',{});
 })
+router.post('*', function (req, res, next){
+	if(req.url == '/favorites'){
+		var page = 'favorites';
+	}else if(req.url == '/losers'){
+		var page = 'losers';
+	}else{
+		res.redirect('/');
+	}
+	MongoClient.connect(mongoURL, function (error, db){
+		db.collection('cars').find({src: req.body.src}).toArray(function (error, result){
+			var updateVotes = function(db, votes, callback){
+				if(page =='favorites'){var newVotes = votes+1;}
+				else if(page == 'losers'){var newVotes = votes - 1;}
+
+				db.collection('cars').updateOne(
+					{ "src" : req.body.src },
+					{
+						$set: {"totalVotes": newVotes},
+						$currentDate: {"lastModified":true}
+					}, function (err, result){
+						callback()
+					})
+			};
+			MongoClient.connect(mongoURL, function (error, db){
+				console.log(result)
+				updateVotes(db,result[0].totalVotes, function() {});
+			})
+		})
+		res.redirect('/');
+	})
+})
+
 
 router.post('/add', function (req,res,next){
-	MongoClient.connect('mongodb://localhost:27017/cardb', function(error, db){
+	MongoClient.connect(mongoURL, function(error, db){
 		console.log(req.body);
 		db.collection('cars').insertOne({
 			name: req.body.name,
-			src: req.body.src
+			src: req.body.src,
+			totalVotes: 0
 		})
 	})
 })
 
-router.post('/favorites', function (req, res, next){
-	MongoClient.connect('mongodb://localhost:27017/cardb', function (error, db){
-		db.collection('users').insertOne({
-			ip: req.ip,
-			vote: req.body.vote,
-			name: req.body.name,
-			image: req.body.src
-		})
-	})
-	res.redirect('../');
-})
+// router.post('/favorites', function (req, res, next){
+// 	MongoClient.connect(mongoURL, function (error, db){
+// 		db.collection('users').insertOne({
+// 			ip: req.ip,
+// 			vote: req.body.vote,
+// 			name: req.body.name,
+// 			image: req.body.src
+// 		})
+// 	})
+// 	res.redirect('../');
+// })
 
 
 module.exports = router;
